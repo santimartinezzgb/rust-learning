0a86abf4331eac6233448209b650b25ed34cc183
santimartinezzgb <santimgb98@gmail.com>
2025-12-12 23:53:56 +0100

    Add 6-mutability

---
+++ 6-mutability/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // VARIABLE INMUTABLE. It doesn´t change
+    // let x = 5;
+    // print!("The value of x is: {}", x);
+    // x = 6;
+    // print!("The value of x is: {}", x);
+
+    // VARIABLE MUTABLE. It can change
+    let mut x = 5;
+    print!("The value of x is: {}\n", x);
+    x = 6;
+    print!("The new value of x is: {}", x);
+}
