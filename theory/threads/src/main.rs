929626fff0e6be1951d3eb8f51eac245d375ee7e
santimartinezzgb <santimgb98@gmail.com>
2025-12-14 00:30:40 +0100

    Update threads/

--- theory/threads/src/main.rs
+++ theory/threads/src/main.rs
@@ -8,6 +8,12 @@ fn main() {
 
     println!("=== Exercise 2: Join Handle ===");
     join_handle();
+
+    println!("=== Exercise 3: Move Closure ===");
+    move_closure();
+
+    println!("=== Exercise 4: Create multiple threads ===");
+    create_multiple_threads();
 }
 
 fn basic_thread() {
@@ -34,4 +40,43 @@ fn join_handle() {
 
     handle.join().unwrap();
     println!("Secondary thread finished");
+}
+
+// 3. Move data to thread
+fn move_closure() {
+    // Array: when you know before the size
+    // Vec: when you need a collection that can grow or shrink
+    let v = vec![1, 2, 3, 4, 5];
+
+    let handle = thread::spawn(move || {
+        for i in v {
+            println!("Vector from thread {}", i);
+            thread::sleep(Duration::from_millis(2000));
+        }
+    });
+
+    handle.join().unwrap();
+}
+
+// 4. Create multiple threads
+fn create_multiple_threads() {
+    let mut handles = vec![];
+
+    for i in 0..5 {
+        // Create 5 threads
+        let handle = thread::spawn(move || {
+            println!("Thread {} working...", i);
+            thread::sleep(Duration::from_millis(2000));
+            i * 2
+        });
+
+        // Push threads to vector
+        handles.push(handle);
+    }
+
+    // Print the thread results
+    for handle in handles {
+        let resultado = handle.join().unwrap();
+        println!("Result: {}", resultado);
+    }
 }
