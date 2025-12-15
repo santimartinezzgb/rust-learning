7f17940d5f28ecaea3f781d702ad2c4f6525fe83
santimartinezzgb <santimgb98@gmail.com>
2025-12-15 12:17:02 +0100

    Update struct_and_impl/

--- theory/struct_and_impl/src/main.rs
+++ theory/struct_and_impl/src/main.rs
@@ -1,11 +1,11 @@
 // use std::io;
 
+#[derive(Debug)]
 struct Book {
     title: String,
     author: String,
     available: bool,
 }
-
 impl Book {
     fn new(title: String, author: String, available: bool) -> Self {
         Book {
@@ -16,13 +16,47 @@ impl Book {
     }
 }
 
+#[derive(Debug)] // This attribute makes the struct printable using {:?}
+struct Library {
+    name: String,
+    books: Vec<Book>,
+}
+impl Library {
+    fn new(name: String) -> Self {
+        Library {
+            name,
+            books: Vec::new(),
+        }
+    }
+    fn add_book(&mut self, book: Book) {
+        self.books.push(book);
+    }
+}
+
 fn main() {
-    let book = Book::new(
+    let mut library = Library::new("Code Library".to_string());
+
+    let book_1 = Book::new(
         "Clean code".to_string(),
         "Robert C. Martin".to_string(),
         true,
     );
-    println!("Book's title: {}\n", book.title);
-    println!("Author's title: {}\n", book.author);
-    println!("Is available?: {}\n", book.available);
+    let book_2 = Book::new(
+        "The Architect".to_string(),
+        "Robert C. Martin".to_string(),
+        false,
+    );
+
+    library.add_book(book_1); // In this moment the book is moved to library, so we can't use it anymore here. This is ownership in action.
+                              // On rust. Any value has a single owner.
+    library.add_book(book_2);
+
+    println!("Library's name: {}", library.name);
+    for book in &library.books {
+        // If you not use & here, the book will be moved from library to this loop, and you can't use library anymore.
+        println!(
+            "Book: {}, Author: {}, Available: {}",
+            book.title, book.author, book.available
+        );
+    }
 }
