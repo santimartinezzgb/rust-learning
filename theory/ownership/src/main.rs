50515a5e71de4605b1d96b3ba2ccc0e845946dad
santimartinezzgb <santimgb98@gmail.com>
2025-12-15 13:08:16 +0100

    Add ownership/

---
+++ theory/ownership/src/main.rs
@@ -0,0 +1,24 @@
+// OWNERSHIP. Basic example of ownership transfer in Rust.
+
+fn main() {
+    {
+        let s1 = String::from("Hello World");
+        let _s2 = s1; // s1 is moved to s2
+                      // _ indicates that s2 is intentionally unused
+
+        // println!("s1 = {}", s1); This line would cause a compile-time error because s1 is no longer valid
+    }
+    {
+        let s1 = String::from("Hello World");
+        let s2 = &s1;
+
+        // When using references (&), ownership is not transferred
+        println!("s1 = {}, s2 = {}", s1, s2);
+    }
+    {
+        let s1 = String::from("Hello World");
+        let s2 = s1.clone(); // s1 is cloned to s2
+
+        println!("s1 = {}, s2 = {}", s1, s2); // Both s1 and s2 are valid
+    }
+}
